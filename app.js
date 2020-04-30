2f97e0980c708e0258b1da11fae4e640366de991
= <=>
2020-04-30 19:34:46 +0530

    Added form for adding new employees

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express      = require('express'),
       app          = express(),
-      bodyParser   = require('body-parser')
+      bodyParser   = require('body-parser'),
       PORT         = 5000
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine","ejs")
 app.use(express.static("public"))
+
+app.get("/", (req,res) => {
+    res.status(200).send("Landing page")
+})
+
+app.get("/employees", (req,res) => {
+    res.render("employees")
+})
+
+app.get("/employees/new", (req,res) => {
+    res.render("add_employee")
+})
 
 app.listen(PORT, () => {
     console.log("Serving app on port", PORT);
--- db/configure.sql
+++ db/configure.sql
@@ -1,48 +1,52 @@
 CREATE DATABASE IF NOT EXISTS dbs;
 USE dbs;
 
-CREATE TABLE IF NOT EXISTS departments (
-	d_id INT AUTO_INCREMENT PRIMARY KEY,
-	d_name VARCHAR(255) NOT NULL,
-	salary DECIMAL NOT NULL
+CREATE TABLE IF NOT EXISTS departments
+(
+    d_id   INT AUTO_INCREMENT PRIMARY KEY,
+    d_name VARCHAR(255) NOT NULL,
+    salary DECIMAL      NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS employees (
-	e_id INT AUTO_INCREMENT PRIMARY KEY,
-    doj DATE,
+CREATE TABLE IF NOT EXISTS employees
+(
+    e_id   INT AUTO_INCREMENT PRIMARY KEY,
+    doj    DATETIME DEFAULT NOW(),
     e_name VARCHAR(255) NOT NULL,
-    d_id INT,
-    FOREIGN KEY (d_id) REFERENCES departments(d_id)
+    d_id   INT,
+    FOREIGN KEY (d_id) REFERENCES departments (d_id)
 );
 
-CREATE TABLE IF NOT EXISTS airlines(
-	a_id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS airlines
+(
+    a_id   INT AUTO_INCREMENT PRIMARY KEY,
     a_name VARCHAR(255) NOT NULL,
-    doj DATE
+    doj    DATE
 );
 
-CREATE TABLE IF NOT EXISTS airlineEmployees (
-	u_id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS airlineEmployees
+(
     e_id INT,
     a_id INT,
-    FOREIGN KEY (e_id) REFERENCES employees(e_id),
-    FOREIGN KEY (a_id) REFERENCES airlines(a_id)
+    FOREIGN KEY (e_id) REFERENCES employees (e_id),
+    FOREIGN KEY (a_id) REFERENCES airlines (a_id)
 );
 
-CREATE TABLE IF NOT EXISTS flights (
-	f_id INT AUTO_INCREMENT PRIMARY KEY,
-    a_id INT,
-    origin VARCHAR(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS flights
+(
+    f_id        INT AUTO_INCREMENT PRIMARY KEY,
+    a_id        INT,
+    origin      VARCHAR(255) NOT NULL,
     destination VARCHAR(255) NOT NULL,
-    f_status VARCHAR(10) NOT NULL,
-    f_time DATETIME NOT NULL DEFAULT NOW(),
-    FOREIGN KEY(a_id) REFERENCES airlines(a_id)
+    f_status    VARCHAR(10)  NOT NULL,
+    f_time      DATETIME     NOT NULL DEFAULT NOW(),
+    FOREIGN KEY (a_id) REFERENCES airlines (a_id)
 );
 
-CREATE TABLE IF NOT EXISTS crew(
-	c_id INT AUTO_INCREMENT PRIMARY KEY,
-    a_id INT,
-    u_id INT,
-	FOREIGN KEY (a_id) REFERENCES airlines(a_id),
-    FOREIGN KEY (u_id) REFERENCES airlineEmployees(u_id)
+CREATE TABLE IF NOT EXISTS crew
+(
+    f_id INT,
+    e_id INT,
+    FOREIGN KEY (f_id) REFERENCES flights (f_id),
+    FOREIGN KEY (e_id) REFERENCES airlineEmployees (e_id)
 );
